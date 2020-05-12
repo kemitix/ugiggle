e04fa817c38d9346e9101c502c29921364a52b37
Paul Campbell <pcampbell@kemitix.net>
2020-05-12 23:38:03 +0100

    service: add ConversionService

---
+++ service/src/main/java/net/kemitix/ugiggle/service/ConversionService.java
@@ -0,0 +1,21 @@
+package net.kemitix.ugiggle.service;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Instance;
+import javax.inject.Inject;
+
+@ApplicationScoped
+public class ConversionService {
+
+    @Inject
+    Instance<AttachmentConverter> attachmentConverters;
+
+    public Attachment convert(Attachment attachment) {
+        return attachmentConverters.stream()
+                .filter(converter -> converter.canHandle(attachment))
+                .findFirst()
+                .flatMap(converter -> converter.convert(attachment))
+                .orElse(attachment);
+    }
+
+}
---
+++ service/src/test/java/net/kemitix/ugiggle/service/ConversionServiceTest.java
@@ -0,0 +1,50 @@
+package net.kemitix.ugiggle.service;
+
+import org.assertj.core.api.WithAssertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import javax.enterprise.inject.Instance;
+
+import java.util.stream.Stream;
+
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class ConversionServiceTest
+        implements WithAssertions {
+
+    private final ConversionService service = new ConversionService();
+
+    private final Instance<AttachmentConverter> converters;
+    private final Attachment attachment;
+
+    public ConversionServiceTest(
+            @Mock Instance<AttachmentConverter> converters,
+            @Mock Attachment attachment
+    ) {
+        this.converters = converters;
+        this.attachment = attachment;
+    }
+
+    @BeforeEach
+    public void setUp() {
+        service.attachmentConverters = converters;
+    }
+
+    @Test
+    @DisplayName("When there are no converters returns the original attachment")
+    public void whenNoConvertersReturnOriginal() {
+        //given
+        given(converters.stream()).willReturn(Stream.empty());
+        //when
+        Attachment result = service.convert(attachment);
+        //then
+        assertThat(result).isSameAs(attachment);
+    }
+}
