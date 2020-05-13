8d3e105c6b1d4d40d4e383c0dab5939bc87b899e
Paul Campbell <pcampbell@kemitix.net>
2020-05-13 08:25:41 +0100

    Attachment: add javadoc and LocalAttachment

--- service/src/main/java/net/kemitix/ugiggle/service/Attachment.java
+++ service/src/main/java/net/kemitix/ugiggle/service/Attachment.java
@@ -3,7 +3,18 @@ package net.kemitix.ugiggle.service;
 import java.io.File;
 
 public interface Attachment {
+
+    /**
+     * The name of the attachment file.
+     *
+     * @return the name of the file
+     */
     File getFileName();
 
+    /**
+     * Downlaods the file to local file system.
+     *
+     * @return the name of the local file.
+     */
     File download();
 }
